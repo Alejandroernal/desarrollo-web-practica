4edba79c1a9218a82219f53bc6799a7924e59899
Alejandro <alebernal06065@gmail.com>
2025-05-27 19:48:49 -0300

    1.7

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -6,17 +6,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const getAll = () => good + neutral + bad
+  const getAverange = () => ((good*1) + (neutral*0) + (bad*-1)) / getAll()
+  const getPositive = () => good*100/getAll()
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+
+
 
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>All {getAll()}</p>
+      <p>Averange {getAverange()}</p>
+      <p>positive {getPositive()}%</p>
+
     </div>
   )
 }
