74c7d075cf8a838e40ce524adfbe194b7c6337f0
Alejandro <alebernal06065@gmail.com>
2025-06-02 22:15:07 -0300

    1.12

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,48 +1,23 @@
 import { useState } from 'react'
-import Statistics from './componnents/Statistics'
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-const [votes, setVotes]= useState({
-  good:0,
-  bad:0,
-  neutral:0
-})
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-)
-
-
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
-
-
-      {/*
-      <button onClick={() => setVotes({... votes, good: votes.good + 1})}>good</button>
-      <button onClick={() => setVotes({... votes, neutral: votes.neutral + 1})}>neutral</button>
-      <button onClick={() => setVotes({... votes, bad:votes.bad + 1})}>bad</button>*/}
-
-      <Button handleClick={() => setVotes({... votes, good: votes.good + 1})} text="good"></Button>
-
-      <Button handleClick={()=> setVotes({... votes, neutral: votes.neutral + 1})} text="neutral"></Button>
-      <Button handleClick={()=> setVotes({... votes, bad: votes.bad + 1})} text="bad"></Button>
-
-
-
-      <h1>statistics</h1>
-      <Statistics votes={votes}></Statistics>
-      
-      {/* (1.7)
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>All {getAll()}</p>
-      <p>Averange {getAverange()}</p>
-      <p>positive {getPositive()}%</p>
-      */}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={() => setSelected(selected + 1)}>Next Anecdote</button>
     </div>
   )
 }
