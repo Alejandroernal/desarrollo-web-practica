d576a2248214316125bdb21a0263f04576838612
Alejandro <alebernal06065@gmail.com>
2025-07-23 11:09:06 -0300

    2.8 Agenta agregamos el telefono y pusimos advertencia en caso de que no se complete un campo

--- parte2-formularios/src/App.jsx
+++ parte2-formularios/src/App.jsx
@@ -2,28 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', tel: '040-123456' },
   ]) 
   const [newName, setNewName] = useState('')
   const [warning, setWarning] = useState('')
+  const [newTel, setNewTel] = useState('')
 
   const handleInputChange = (event) => {
     setNewName(event.target.value)
     setWarning('') // Limpia la advertencia al escribir
   }
 
+  const handleTelChange = (event) => {
+    setNewTel(event.target.value)
+    setWarning('') // Limpia la advertencia al escribir
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    if (newName.trim() === ''){
-      alert('Name cannot be empty')
+    if (newName.trim() === '' || newTel.trim() === '') {
+      alert('Name or Tel cannot be empty')
       return
     }
     if (persons.some(person => person.name === newName)) {
       alert(` ${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({ name: newName }))
+    setPersons(persons.concat({ name: newName , tel: newTel }))
     setNewName('')
+    setNewTel('')
     setWarning('')
   }
 
@@ -35,13 +42,16 @@ const App = () => {
           name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
+          number: <input value={newTel} onChange={handleTelChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, idx) => (
-          <li key={idx}>{person.name}</li>
+          <li key={idx}>{person.name} {person.tel}</li>
         ))}
       </ul>
     </div>
